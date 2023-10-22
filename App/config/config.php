35ce24ea172415d29c6fdfdc8f70e2959c8b4dfb
Kinn-01 <gilmanarief23@gmail.com>
2023-10-22 11:22:35 +0700

    Video 8 : Database Wrapper

---
+++ App/config/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+
+define('BASEURL', 'http://localhost/prakweb2023_b_213040037_mvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
